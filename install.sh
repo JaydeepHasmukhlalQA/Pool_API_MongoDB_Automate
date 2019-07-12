1913455d90c9e86cf1bd50a5212a267ff0b3e10f
Jaydeep <jaydeep.hasmukhlal@academytrainee.com>
2019-07-12 13:18:14 +0100

    Created script to install and run mongodb.

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CYAN="\033[0;36"
+GREEN="\033[0;32m"
+DEFULT="\e[39mDefault"
+
+echo -e "${CYAN}Installing MongoDB.${DEFULT}"
+sudo apt-get install -y mongodb
+echo -e "${GREEN}Finished updating.${DEFULT}"
+
+echo -e "${CYAN}Opening IP's of MongoDB.${DEFULT}"
+sed -i "/bind_ip = 127.0.0.1/c\bind_ip = 0.0.0.0" /etc/mongodb.conf
+sed -i "/#port = 27017/c\port = 27017" /etc/mongodb.conf
+echo -e "${CYAN}Done.${DEFULT}"
+
+echo -e "${CYAN}Starting MongoDB.${DEFULT}"
+sudo systemctl daemon-reload
+sudo systemctl start MongoDB
+echo -e "${CYAN}Started MongoDB.${DEFULT}"
